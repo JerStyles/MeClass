c926bdcd5b3d417e32081053bc440e56b1e31cf2
Chungche Huang <108234230+JerStyles@users.noreply.github.com>
2023-11-27 01:02:17 +0700

    Temp remove full screen icon in wowchemy-slides.js

--- assets/js/wowchemy-slides.js
+++ assets/js/wowchemy-slides.js
@@ -130,13 +130,13 @@ Reveal.initialize({
         title: 'Toggle overview (O)',
         icon: '<i class="fa fa-th"></i>',
         action: 'Reveal.toggleOverview();'
-      },
-      {
-        id: 'full-screen',
-        icon: '<i class="fa-solid fa-expand"></i>',
-        title: 'Toggle Full Screen',
-        action: 'screenfull.toggle();'
-      }
+       } // ,
+      // {
+      //   id: 'full-screen',
+      //   icon: '<i class="fa-solid fa-expand"></i>',
+      //   title: 'Toggle Full Screen',
+      //   action: 'screenfull.toggle();'
+      // }
     ]
   },
   chalkboard: {
